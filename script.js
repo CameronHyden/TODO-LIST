c36b1e95af29436c59b62a02e7bb02497aaaf9eb
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-03-01 15:54:35 +0000

    function-added-to-remove-and-add-text

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const nameBox = document.querySelector("#user__textBox");
-const resetButton = document.querySelectorAll(".buttons")
+const resetButton = document.querySelectorAll(".buttons");
+const inputLabel = document.querySelector(".input-label");
+const homepageInfo = document.querySelector("#users__text");
+
+
+
+const clearInputBoxLabel = ( ) =>{
+    inputLabel.innerHTML = ""
+}
+
+const newScreen = (event) =>{
+    if (event.keyCode === 13){
+        homepageInfo.style.display =('block');
+    }
+}
+
+
+
+
+nameBox.addEventListener("click", clearInputBoxLabel);
+nameBox.addEventListener("keyup", newScreen);
